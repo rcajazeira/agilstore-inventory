840f9ac3750ddaffc16a7f9fab38d09a51ba71da
Rafael Cajazeira <147667168+rcajazeira@users.noreply.github.com>
2026-01-06 15:23:24 -0300

    Constrtuctor de products

--- src/products.js
+++ src/products.js
@@ -1,3 +1,9 @@
 // products.js
 class Product {
+    constructor(id, name, category, quantity, price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.quantity = parseInt(quantity);
+        this.price = parseFloat(price);
 }
